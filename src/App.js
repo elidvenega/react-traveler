71f26e120aeb12575b0ebf4e4a27cf6183bb68ed
elid venega <venegaelid@gmail.com>
2020-09-24 21:42:14 -0700

    pages added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,11 @@ import { BrowserRouter as Router, Switch, Route } from
 'react-router-dom';
 import './App.css';
 import Home from './components/pages/Home';
+import Services from './components/pages/Services';
+import Products from './components/pages/Products';
+import SignUp from './components/pages/SignUp';
+
+
 
 function App() {
   return (
@@ -11,6 +16,9 @@ function App() {
       <Router>
       <Navbar />
       <Route path='/' exact component={Home}/>
+      <Route path='/services' exact component={Services}/>
+      <Route path='/products' exact component={Products}/>
+      <Route path='/sign-up' exact component={SignUp}/>
       </Router>
     </div>
   );
